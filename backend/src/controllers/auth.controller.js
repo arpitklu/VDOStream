7b5c58e5479d2413e86fe90f2ad7523cef40cbf8
arpitklu <arrpiit2004@gmail.com>
2025-08-02 00:52:11 +0530

    feat: Login and Logout Endpoints created with User creation in Stream

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { useImperativeHandle } from "../lib/stream.js";
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js"
 import jwt from "jsonwebtoken";
 
