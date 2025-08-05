87f1d51b4267f8c442b8c13609f18585ee459ca9
arpitklu <arrpiit2004@gmail.com>
2025-08-05 10:58:00 +0530

    feat: Sending Friend Request Configured git add .

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller.js";
+import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.use(protectRoute);
 
 router.get("/", protectRoute, getRecommendedUsers);
 router.get("/friends", protectRoute, getMyFriends);
+
+router.post("/friend-request/:id" , sendFriendRequest);
 
 export default router;
