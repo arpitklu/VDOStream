19e852aaddbc6f26748e75695b869c737c2a79f7
arpitklu <arrpiit2004@gmail.com>
2025-08-09 19:41:33 +0530

    feat: Tanstack query setup done.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -13,6 +14,10 @@ const app = express()
 dotenv.config()
 const PORT = process.env.PORT
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true //allow frontend to send the cookies.
+}))
 app.use(express.json());
 app.use(cookieParser());
 
